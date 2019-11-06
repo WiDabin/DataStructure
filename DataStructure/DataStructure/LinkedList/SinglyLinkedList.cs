3ade9d8aaba14f6aef9037f8cc9fb08b4f4951cc
widalida26 <widalida26@gmail.com>
2019-11-07 02:28:38 +0900

    add: basic singly linked list
    - create singly linked list class
    - implement singly linked list node

---
+++ DataStructure/DataStructure/LinkedList/SinglyLinkedList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure.LinkedList
+{
+    public class SinglyLinkedListNode<T>
+    {
+        public T Data { get; set; }
+        public SinglyLinkedListNode<T> Next { get; set; }
+        
+        public SinglyLinkedListNode(T data)
+        {
+            this.Data = data;
+            this.Next = null;
+        }
+    }
+}
